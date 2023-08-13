7ea322e3dc4bd2022dbe13fd9a53283ca568475a
ONF-Production <108579665+ONF-Production@users.noreply.github.com>
2023-08-13 09:55:14 -0300

    configuracion wsgi

--- servicios/settings/production.py
+++ servicios/settings/production.py
@@ -1,1 +1,10 @@
 from .base import *
+
+
+DATABASES = {
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
